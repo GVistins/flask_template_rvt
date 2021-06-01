427d833256a939e942be37ee140b7eecb5e45d03
DP2GVistins <>
2021-06-01 13:40:40 +0000

    Izveidoju ļoti daudz izmaiņas mājaslapai un pārveidoju vecos flask failus

--- main.py
+++ main.py
@@ -7,13 +7,21 @@ def index():
   return render_template("index.html")
 
 
-@app.route('/about')
-def about():
-  return render_template("about.html")
+@app.route('/agenturas')
+def agenturas():
+  return render_template("agenturas.html")
 
-@app.route('/kontakti')
-def kontakti():
-  return render_template("kontakti.html")
+@app.route('/valstis')
+def valstis():
+  return render_template("valstis.html")
+
+@app.route("/celojumi")
+def celojumi():
+  return render_template("celojumi.html")
+
+@app.route("/stats")
+def stats():
+  return render_template("stats.html")
 
 
 app.run(host='0.0.0.0', port=8080)
